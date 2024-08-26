e064eaf4202d1ad9a3a04d153367351b382d9dde
gihoon <kiki01111@naver.com>
2024-08-26 18:35:10 +0900

    Refactor: useSelctedModal.ts 내부 recoil -> zustand , TODO: selectedModalAtom zustand교체

--- src/components/ModalContainer/ModalContainer.tsx
+++ src/components/ModalContainer/ModalContainer.tsx
@@ -1,19 +1,20 @@
 import styled from 'styled-components'
-import React, { RefObject } from 'react'
-import { useRecoilState, useRecoilValue } from 'recoil'
-import { modalStateAtom } from '../../recoil/modalStateAtom';
+import  { RefObject } from 'react'
+import {  useRecoilValue } from 'recoil'
+// import { modalStateAtom } from '../../recoil/modalStateAtom';
 import selectedModalAtom from '../../recoil/selectedModalAtom';
 import LoginModal from '../ModalComponents/LoginModal';
 import DefaultModal from '../ModalComponents/DefaultModal';
+import { useModalStore } from '../../store/useModalStore';
 
 
-interface ModalStateType{
-    modalState:boolean;
+interface IsModalOpenType{
+  isModalOpen:boolean;
   }
   
 const ModalOutSide=styled.div.withConfig({
-    shouldForwardProp: (prop) => prop !== 'modalState'
-  })<ModalStateType>`
+    shouldForwardProp: (prop) => prop !== 'isModalOpen'
+  })<IsModalOpenType>`
     width: 100vw;
     height: 100vh;
     position: fixed;
@@ -24,7 +25,7 @@ const ModalOutSide=styled.div.withConfig({
     justify-content: center;
     align-items: center;
     display: none;
-    display: ${(props)=>props.modalState?'flex':'none'};
+    display: ${(props)=>props.isModalOpen?'flex':'none'};
   `
 
 const ModalBodyWrapper=styled.div`
@@ -48,11 +49,14 @@ interface ModalContainerProps{
 }
 
 const ModalContainer= ({modalRef}:ModalContainerProps) => {
-    const [modalState,setModalState]=useRecoilState(modalStateAtom);
+
+    // const [modalState,setModalState]=useRecoilState(modalStateAtom);
+    const{isModalOpen,setModalState}=useModalStore();
+    //모달 네이밍에 따른 컴포넌트 렌더링
     const selectedModalName=useRecoilValue(selectedModalAtom);
 
     return (
-            <ModalOutSide ref={modalRef} modalState={modalState} >
+            <ModalOutSide ref={modalRef} isModalOpen={isModalOpen} >
                 <ModalBodyWrapper >
                     <ModalCloseBtn onClick={()=>setModalState(false)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 80c-8.8 0-16 7.2-16 16l0 320c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-320c0-8.8-7.2-16-16-16L64 80zM0 96C0 60.7 28.7 32 64 32l384 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zm175 79c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></ModalCloseBtn>
                     <>
--- src/hooks/useSelectedModal.ts
+++ src/hooks/useSelectedModal.ts
@@ -1,13 +1,18 @@
 import { useSetRecoilState } from "recoil";
-import { modalStateAtom } from "../recoil/modalStateAtom";
+// import { modalStateAtom } from "../recoil/modalStateAtom";
 import selectedModalAtom from "../recoil/selectedModalAtom";
+import { useModalStore } from "../store/useModalStore";
 
 
 const useSelectedModal=(modalName:string)=>{
-    const modalState=useSetRecoilState(modalStateAtom);
+
+    // const modalState=useSetRecoilState(modalStateAtom);
+    const {setModalState}=useModalStore();
     const setSeledtedModal=useSetRecoilState(selectedModalAtom);
+
     const updateModalName=()=>{
-        modalState(true);
+        // modalState(true);
+        setModalState(true);
         setSeledtedModal(modalName);
     }
     return {updateModalName};
